e55c167eb83dff825c7b7d47c9eeb927469c52ed
EricLod <eric.wiehman@lodicon.co.za>
2023-04-14 14:14:34 +0200

    Day 24: Add Footer and Headers

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from fpdf import FPDF
 import pandas as pd
 
+def footHead(ln):
+    pdf.ln(ln)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+    pass
+
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
@@ -12,7 +19,14 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 20, 200, 20)
 
-    for i in range(row["Pages"]):
+    #Set the footer
+    footHead(ln=240)
+
+    for i in range(row["Pages"]-1):
         pdf.add_page()
 
+        #Set the header
+        footHead(ln=20)
+
 pdf.output("output.pdf")
+
