75e0aabaed24a8642c7b8e0eb0512d5df3615032
EricLod <eric.wiehman@lodicon.co.za>
2023-04-17 10:55:07 +0200

    Day 24: Add lines to pages

--- main.py
+++ main.py
@@ -8,6 +8,12 @@ def footHead(ln):
     pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
     pass
 
+def addLines():
+    i = 20
+    while i < 270:
+        pdf.line(10, i, 200, i)
+        i = i + 10
+
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 df = pd.read_csv("topics.csv")
@@ -17,16 +23,17 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=12)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10, 20, 200, 20)
+    pdf.line(10, 19, 200, 19)
+    addLines()
 
     #Set the footer
     footHead(ln=240)
 
     for i in range(row["Pages"]-1):
         pdf.add_page()
-
+        pdf.line(10, 19, 200, 19)
+        addLines()
         #Set the header
-        footHead(ln=20)
+        footHead(ln=0)
 
 pdf.output("output.pdf")
-
